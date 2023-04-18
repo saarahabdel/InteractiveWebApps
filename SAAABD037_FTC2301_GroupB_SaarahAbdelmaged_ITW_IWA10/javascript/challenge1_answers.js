2bac50ff0506dfd90324fa912c8f4c135388e7e1
saarahabdel <saarahabdel15@gmail.com>
2023-04-18 12:52:09 +0200

    Edited IWA10

---
+++ SAAABD037_FTC2301_GroupB_SaarahAbdelmaged_ITW_IWA10/javascript/challenge1_answers.js
@@ -0,0 +1,88 @@
+const currentYear = new Date().getFullYear()
+
+const holidays = {
+    0: {
+        id: 0,
+        name: 'Day of Reconciliation',
+        date: `16 December ${currentYear}`,
+    },
+    1: {
+        id: 1,
+        name: 'Workers Day',
+        date: new Date(`1 April ${currentYear}`),
+    },
+    2: {
+        id: 2,
+        name: 'Day of Goodwill',
+        date: new Date(`26 December ${currentYear}`),
+    },
+    3: {
+        id: 3,
+        name: 'New Year Day',
+        date: new Date(`1 January ${currentYear}`),
+    },
+    4: {
+        id: 4,
+        name: 'Womens Day',
+        date: new Date(`9 August ${currentYear}`),
+    },
+    5: {
+        id: 5,
+        name: 'Heritage Day',
+        date: new Date(`24 September ${currentYear}`),
+    },
+    6: {
+        id: 6,
+        name: 'Christmas Day',
+        date: new Date(`25 December ${currentYear} 13:25`),
+    },
+    7: {
+        id: 7,
+        name: 'Youth Day',
+        date: new Date(`16 June ${currentYear}`),
+    },
+    8: {
+        id: 8,
+        name: 'Human Rights Day',
+        date: new Date(`21 March ${currentYear}`)
+    },
+}
+
+const christmas = 6
+const futureId = 9
+
+/*console.log("hello");
+
+console.log(holidays[0]);*/
+
+if (holidays[9] === true) {
+
+    console.log(holidays.name);
+} else {
+
+    console.log("ID 9 not created")
+
+}
+
+let copied = {
+
+    6: {
+        id: 6,
+        name: 'X-mas Day',
+        date: new Date(`25 December ${currentYear} 00:00:00`),
+    }
+}
+
+if (holidays.date > copied.date) {
+
+    console.log("New date is earlier: true")
+
+} else {
+
+    console.log("New date is earlier: false")
+
+}
+
+
+
+
