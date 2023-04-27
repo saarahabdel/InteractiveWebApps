ffed1ed0ee3cdddb593b225b9c56dfa2c78d695d
saarahabdel <saarahabdel15@gmail.com>
2023-04-27 12:38:14 +0200

    Attempted IWA11 again

--- SAAABD037_FTC2301_GroupB_SaarahAbdelmaged_ITW_IWA11/javascript/challenge1_answers.js
+++ SAAABD037_FTC2301_GroupB_SaarahAbdelmaged_ITW_IWA11/javascript/challenge1_answers.js
@@ -1,8 +1,12 @@
 const root1 = document.querySelector('[data-key="order1"]');
-const biscuits1 = document.querySelector('[data-key="order1"] .biscuits .count');
+const biscuits1 = document.querySelector('[data-key="order1"] .biscuits');
 const donuts1 = document.querySelector('[data-key="order1"] .donuts');
 const pancakes1 = document.querySelector('[data-key="order1"] .pancakes');
 const status1 = document.querySelector('[data-key="order1"] .status')
+
+console.log(status1);
+
+
 
 
 
