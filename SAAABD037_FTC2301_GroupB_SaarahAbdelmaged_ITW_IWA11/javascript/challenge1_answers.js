63da177a50544cc4d45b90e38d2d4f6c9ab4a59f
saarahabdel <saarahabdel15@gmail.com>
2023-04-27 20:51:06 +0200

    Edited

--- SAAABD037_FTC2301_GroupB_SaarahAbdelmaged_ITW_IWA11/javascript/challenge1_answers.js
+++ SAAABD037_FTC2301_GroupB_SaarahAbdelmaged_ITW_IWA11/javascript/challenge1_answers.js
@@ -1,10 +1,18 @@
-const root1 = document.querySelector('[data-key="order1"]');
-const biscuits1 = document.querySelector('[data-key="order1"] .biscuits');
-const donuts1 = document.querySelector('[data-key="order1"] .donuts');
-const pancakes1 = document.querySelector('[data-key="order1"] .pancakes');
-const status1 = document.querySelector('[data-key="order1"] .status')
+// const root1 = document.querySelector('[data-key="order1"]');
+// const biscuits1 = document.querySelector('[data-key="order1"] .biscuits');
+// const donuts1 = document.querySelector('[data-key="order1"] .donuts');
+// const pancakes1 = document.querySelector('[data-key="order1"] .pancakes');
+// const status1 = document.querySelector('[data-key="order1"] .status')
 
-console.log(status1);
+// console.log(status1);
+
+const root1 = document.querySelector('[data-key="order1"]');
+const biscuits1 = document.querySelector('[data-key="order1"] .biscuits .count');
+
+console.log(root1);
+console.log(biscuits1);
+
+console.log(biscuits1.setAttribute('count', 'root1'));
 
 
 
