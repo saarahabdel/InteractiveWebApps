c57f65512ccd15125813ca53ae281f1bf9e954ec
saarahabdel <saarahabdel15@gmail.com>
2023-05-02 18:50:53 +0200

    Edited

--- SAAABD037_FTC2301_GroupB_SaarahAbdelmaged_ITW_IWA14/challenge1/javascript/challenge1.js
+++ SAAABD037_FTC2301_GroupB_SaarahAbdelmaged_ITW_IWA14/challenge1/javascript/challenge1.js
@@ -2,14 +2,13 @@ firstName = 'John';
 age = 35;
 occupation = 'Coding';
 
-const logTwice = () => {
-  console.log(hobby)
-  console.log(hobby)
-}
+const logTwice = (firstName, age, occupation) => {
+  console.log((`Hello, ${firstName} (${age}). I love ${occupation}!`))
+};
 
-function hobby () {
-  logTwice(`Hello, ${firstName} (${age}). I love ${occupation}!`)
-  return logTwice
-}
+// function hobby () {
+//   logTwice(`Hello, ${firstName} (${age}). I love ${occupation}!`)
+//   return logTwice
+// }
 
-console.log(hobby())
+// console.log(hobby())
