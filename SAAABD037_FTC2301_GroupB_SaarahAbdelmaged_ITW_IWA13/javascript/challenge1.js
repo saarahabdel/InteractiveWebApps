886d3ce24d4f03a1eb605ff3ca2571e23b4ca232
saarahabdel <saarahabdel15@gmail.com>
2023-05-02 15:51:05 +0200

    Attempted challenge 13 again

--- SAAABD037_FTC2301_GroupB_SaarahAbdelmaged_ITW_IWA13/javascript/challenge1.js
+++ SAAABD037_FTC2301_GroupB_SaarahAbdelmaged_ITW_IWA13/javascript/challenge1.js
@@ -4,49 +4,55 @@ let calculated = '1'
 
 // Only allowed to change below
 
-const logCalc = () => { 
-    const isString = typeof calculated == 'numerical-string';
-    const calculatedAsNumber = isString ? calculated : parseInt(calculated);
+
+const logCalc = (calculated) => { 
+  const isString = typeof calculated == 'numerical-string';
+  const calculatedAsNumber = isString ? calculated : parseInt(calculated);
     calculated = calculatedAsNumber + 1;
-    console.log(calculated)
-    return calculated
+    //console.log(calculated)
+    return calculated  
 };
+const result = logCalc(calculated)
+console.log(result)
+//console.log(logCalc())
 
-
-// console.log(logCalc())
-
-const calcUser = () => {
-  if(calculated >= 2) user='John';
-  if(calculated > 2){state = 'requesting'};
-  if(calculated > 3){state = 'idle'};
-  console.log(user)
-  console.log(state)
+const calcUser = (result) => {
+  if(result >= 2) {
+    return user='john'
+  };
+  if(result >= 2){
+    return state = 'requesting'
+  } else {
+    return state = 'idle'
+  };
 };
+console.log(user)
+console.log(state)
 console.log(calcUser())
 
-const checkUser = () => {
-	if (user && state === 'requesting') {
-		console.log(`User: ${user} (${calculated})`)
-	}
-  return checkUser
-}
+// const checkUser = () => {
+// 	if (user && state === 'requesting') {
+// 		console.log(`User: ${user} (${calculated})`)
+// 	}
+//   return checkUser
+// }
 
-// Only allowed to change code above
+// // Only allowed to change code above
 
-checkUser()
-calcUser()
+// checkUser()
+// calcUser()
 
-checkUser()
-calcUser()
+// checkUser()
+// calcUser()
 
-checkUser()
-calcUser()
+// checkUser()
+// calcUser()
 
-checkUser()
-calcUser()
+// checkUser()
+// calcUser()
 
-checkUser()
-calcUser()
+// checkUser()
+// calcUser()
 
 
 
