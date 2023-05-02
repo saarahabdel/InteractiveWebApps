dfe258f62464d872b25040a1c65d7489cb9ffd07
saarahabdel <saarahabdel15@gmail.com>
2023-05-02 21:50:56 +0200

    Completed IWA14 Challenge

--- SAAABD037_FTC2301_GroupB_SaarahAbdelmaged_ITW_IWA14/challenge1/javascript/challenge1_answers.js
+++ SAAABD037_FTC2301_GroupB_SaarahAbdelmaged_ITW_IWA14/challenge1/javascript/challenge1_answers.js
@@ -2,3 +2,13 @@ let firstName = 'John';
 let age = 35;
 let hobby = 'Coding';
 
+const logTwice = (parameter) => { // had to pass the parameter in () to call it in code block
+    console.log(parameter)
+    console.log(parameter)
+}
+
+function hobby1 () {  // changed function name bc was same as hobby variable
+    logTwice(`Hello, ${name} (${age}). I love ${hobby}!`)
+}
+
+hobby1()
