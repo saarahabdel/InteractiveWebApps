24f14d779b0eea05a0618abaa19fcaef73e37aea
saarahabdel <saarahabdel15@gmail.com>
2023-05-04 11:55:06 +0200

    Completed IWA18 Challenge

--- SAAABD037_FTC2301_GroupB_SaarahAbdelmaged_ITW_IWA18/javascript/scripts.js
+++ SAAABD037_FTC2301_GroupB_SaarahAbdelmaged_ITW_IWA18/javascript/scripts.js
@@ -1,3 +1,6 @@
+import { TABLES, COLUMNS, state, createOrderData, updateDragging } from './data.js'
+import { createOrderHtml, html, updateDraggingHtml, moveToColumn } from './view.js'
+
 /**
  * A handler that fires when a user drags over any element inside a column. In
  * order to determine which column the user is dragging over the entire event
@@ -27,15 +30,148 @@ const handleDragOver = (event) => {
     updateDraggingHtml({ over: column })
 }
 
+let beingDragged;
+let id;
 
-const handleDragStart = (event) => {}
-const handleDragEnd = (event) => {}
-const handleHelpToggle = (event) => {}
-const handleAddToggle = (event) => {}
-const handleAddSubmit = (event) => {}
-const handleEditToggle = (event) => {}
-const handleEditSubmit = (event) => {}
-const handleDelete = (event) => {}
+/**
+ * This function is called when a user starts dragging an order.
+ * It sets the active dragging column in the state object (data.js) 
+ * to the column over which the order was originally located.
+ * @param {Event} event 
+ */
+const handleDragStart = (event) => {
+    beingDragged = event.target.closest('.order')
+    state.dragging.source = state.dragging.over
+    id = beingDragged.dataset.id
+}
+
+/**
+ * This function is called when a user stops dragging an order.
+ * It moves the order to the new column and updates the HTML.
+ * @param {Event} event 
+ */
+const handleDragEnd = (event) => {
+    event.preventDefault()
+    const move = state.dragging.over
+    moveToColumn(id, move)
+    updateDraggingHtml({ over: column })
+}
+
+/**
+ * This function is called when a user clicks on the "?" button 
+ * and when a user clicks on the "Close" button.
+ * The "?" button opens the modal and the "Close" button closes it.
+ * @param {Event} event 
+ */
+const handleHelpToggle = (event) => {
+    const { target } = event;
+
+    if (target == html.other.help) {
+        html.help.overlay.showModal()
+    }
+
+    if (target == html.help.cancel) {
+        html.help.overlay.close()
+    }
+}
+
+/**
+ * This function is called when a user clicks on the "Add Order"
+ * button and when a user clicks on the "Cancel" button.
+ * The "Add Order" button opens the modal and the "Cancel" button closes it.
+ * @param {Event} event 
+ */
+const handleAddToggle = (event) => {
+    const { target } = event;
+
+    if (target == html.other.add) {
+        html.add.overlay.showModal()
+    }
+
+    if (target == html.add.cancel) {
+        html.add.overlay.close()
+    }
+}
+
+/**
+ * This function is called when a user submits the order.
+ * It extracts the data entered in the form, creates an order data object,
+ * updates the state with the new order data, creates an HTML element for
+ * the new order, and appends the HTML element.
+ * .value is used to access the 'title' and 'table' input.
+ * @param {Event} event 
+ */
+const handleAddSubmit = (event) => {
+    event.preventDefault()
+    const title = event.target.elements.title.value
+    const table = event.target.elements.table.value
+    const column = 'ordered'
+    const order = createOrderData({ title, table, column })
+    state.orders[order.id] = order
+    const element = createOrderHtml(order)
+    const columnContainer = document.querySelector(`[data-column="${column}"]`)
+    columnContainer.appendChild(element)
+    html.add.overlay.close()
+}   
+
+/**
+ * This function is called when a user clicks on an order (that was already created).
+ * When the user clicks on the order it opens the modal.
+ * @param {Event} event 
+ */
+const handleEditToggle = (event) => {
+    const { target } = event;
+
+    const editOpen = document.querySelector('.order')
+
+    if (target == editOpen) {
+        html.edit.overlay.showModal()
+    }
+
+    if (target == html.edit.cancel) {
+        html.edit.overlay.close()
+    }
+}
+
+/**
+ * This function is called when a user submits the "Edit Order".
+ * It extracts the data entered in the form, removes the old order,
+ * creates a new order data object, creates a new HTML element, and
+ * appends the HTML element.
+ * .value is used to access the 'title', 'table', and 'column' input.
+ * @param {Event} event 
+ */
+const handleEditSubmit = (event) => {
+    event.preventDefault();
+    const order = document.querySelector('.order')
+    order.remove()
+    const data = {
+        title: html.edit.title.value,
+        table: html.edit.table.value,
+        column: html.edit.column.value,
+    }
+    const orderData = createOrderData(data)
+    const orderHtml = createOrderHtml(orderData)
+    const columns = document.querySelector(`[data-column="${data.column}"]`)
+    columns.appendChild(orderHtml)
+    html.edit.overlay.close()
+}
+
+/**
+ * This function is called when a user clicks "delete" on
+ * the "Edit Order" form.
+ * It removes the order and close the modal.
+ * @param {Event} event 
+ */
+const handleDelete = (event) => {
+    const { target } = event
+
+    if (target == html.edit.delete) {
+        document.querySelector('.order').remove()
+    }
+
+    html.edit.overlay.close()
+}
 
 html.add.cancel.addEventListener('click', handleAddToggle)
 html.other.add.addEventListener('click', handleAddToggle)
@@ -57,3 +193,4 @@ for (const htmlColumn of Object.values(html.columns)) {
 for (const htmlArea of Object.values(html.area)) {
     htmlArea.addEventListener('dragover', handleDragOver)
 }
+
