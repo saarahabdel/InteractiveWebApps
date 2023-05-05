be4fcb7b8937f94ac7f8553adf8d039b3bb09cd8
saarahabdel <saarahabdel15@gmail.com>
2023-05-05 10:26:06 +0200

    Edited

---
+++ IWA18-LECTURE/script.js
@@ -0,0 +1,9 @@
+// HOW TO ADD EVENT LISTENER 
+
+// <button id="example"> Click Me! </button> 
+
+// create our handler::                  buttonHandler = () => console.log('The button was clicked');   <--- Function that occurs when event happens
+
+// const button = document.querySelector("#example");
+// example.addEventListener('click', buttonHandler)    // schalk is the event 
+
