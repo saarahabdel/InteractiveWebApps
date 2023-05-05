04e04f5224dc5f7485b9d15d865ff6b7f3a512fb
saarahabdel <saarahabdel15@gmail.com>
2023-05-05 12:59:17 +0200

    Edited

--- IWA18-LECTURE/script.js
+++ IWA18-LECTURE/script.js
@@ -7,17 +7,28 @@
 // const button = document.querySelector("#example");
 // example.addEventListener('click', buttonHandler)    // schalk is the event 
 
-const example = document.querySelector('#example');
-const exampleHandler = () => {
-    console.log('The button was clicked!')
-    example.innerText = 'Clicked.'                    // changes button text to clicked once clicked 
-    example.disabled = true                           // disables button once clicked 
-};
+// const example = document.querySelector('#example');
+// const exampleHandler = () => {
+//     console.log('The button was clicked!')
+//     example.innerText = 'Clicked.'                    // changes button text to clicked once clicked 
+//     example.disabled = true                           // disables button once clicked 
+// };
 
 
-example.addEventListener('click', exampleHandler);
+// example.addEventListener('click', exampleHandler);
+
+// example.addEventListener(
+//     'click',
+//     (event) => {
+//         console.log(event)
+//     }
+// )
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+
+
+
 
 
 // If you have 10 buttons in HTML with no id's ::
@@ -27,17 +38,6 @@ example.addEventListener('click', exampleHandler);
 // - bc its not a real array, its a node list:
 // - we use array from to create an array from the node list that gets returned 
 
-const handler = () => {
-    console.log('clicked!')
-}
-// - our event listener
-
-for (const button of buttonsArray) {
-    button.addEventListener('click', handler)
-
-}
-// - a for loop to iterate over buttons array 
-// - for each button that we loop over, add event listener 
 
 const BACKGROUNDS = [
     'red',
@@ -50,12 +50,57 @@ const BACKGROUNDS = [
 // - when someone clicks a button we want to assign it a random color 
 // - this random color we can grab from an array above (BACKGROUNDS array)
 
+const buttonsList = document.querySelectorAll('button')
+const buttonsArray = Array.from(buttonsList)
+
+/**
+ * Returns a random color value, based on a random index used on 
+ * {@link BACKGROUNDS}
+ * @returns {string}
+ */
 const calcRandomColor = () => {
 
+    const maxIndex = BACKGROUNDS.length - 1            // calculate max length of the array    index always 1 shorter than actual array
+    const randomIndex = Math.round(maxIndex * Math.random())  // get a random index based off the amount the index can be 
+    return BACKGROUNDS[randomIndex]                           // apply that index to backgrounds then return the value 
 }
+
+
+const handler = (event) => {
+    const { target } = event  // this will give the HTML node       // look at the target the event was dispatched from, use destructuring
+    const { override } = target.dataset || { color: null }              // 1)attempt to destructure color   2) target the data set 
+    target.style.color = 'white'   // color of the text
+    target.style.background = override || calcRandomColor()        // if there is color, use the color, otherwise fallback to calcRandomColor           
+
+    console.log('clicked!')
+
+    // disable this behavior once selected a button (only ever enable one button)
+    // loop over this again
+    for (const button of buttonsArray) {
+        button.removeEventListener('click', handler)
+    
+    } 
+
+
+}
+// - our event listener
+
+for (const button of buttonsArray) {               // commented this event listener and pasted in const handler
+    button.addEventListener('click', handler)      // useful to declare event listeners as functions outside of where you register it                                                  // in order to remove it again
+}                                                  // bc you need that same function in order to remove it again           
+// - a for loop to iterate over buttons array 
+// - for each button that we loop over, add event listener 
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------- //
 
 
 // SCROLLING: 
 window.addEventListener('scroll', () => console.log('Scrolled'))
+
+// THE EVENT OBJECT 
+// - all events have event objects 
+
+
 
 
