2873ba90221035fc57e0203b01a2c522c83fd6e8
saarahabdel <saarahabdel15@gmail.com>
2023-05-05 11:03:34 +0200

    Edited

--- IWA18-LECTURE/script.js
+++ IWA18-LECTURE/script.js
@@ -7,3 +7,55 @@
 // const button = document.querySelector("#example");
 // example.addEventListener('click', buttonHandler)    // schalk is the event 
 
+const example = document.querySelector('#example');
+const exampleHandler = () => {
+    console.log('The button was clicked!')
+    example.innerText = 'Clicked.'                    // changes button text to clicked once clicked 
+    example.disabled = true                           // disables button once clicked 
+};
+
+
+example.addEventListener('click', exampleHandler);
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+
+// If you have 10 buttons in HTML with no id's ::
+
+// const buttonsArray = Array.from(document.querySelectorAll('button')) 
+// - use querySelectorAll to get all our buttons on our page 
+// - bc its not a real array, its a node list:
+// - we use array from to create an array from the node list that gets returned 
+
+const handler = () => {
+    console.log('clicked!')
+}
+// - our event listener
+
+for (const button of buttonsArray) {
+    button.addEventListener('click', handler)
+
+}
+// - a for loop to iterate over buttons array 
+// - for each button that we loop over, add event listener 
+
+const BACKGROUNDS = [
+    'red',
+    'blue',
+    'green',
+    'purple',
+    'brown',
+    'black'
+]
+// - when someone clicks a button we want to assign it a random color 
+// - this random color we can grab from an array above (BACKGROUNDS array)
+
+const calcRandomColor = () => {
+
+}
+
+
+// SCROLLING: 
+window.addEventListener('scroll', () => console.log('Scrolled'))
+
+
